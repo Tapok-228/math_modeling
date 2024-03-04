c7b742dec1f98f2eef57b7550ab7637f951a9547
Tapok-228 <153547706+Tapok-228@users.noreply.github.com>
2024-03-04 15:50:26 +0000

    uhiuh

---
+++ lab_6_task1.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+x = [1, 1, 5, 5, 1]
+y = [1, 5, 5, 1, 1]
+
+plt.plot(x, y, marker='o', ms=15)
+plt.axis('equal')
+
+plt.savefig('fig_6.png')
---
+++ lec_6_pyplot.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+
+plt.plot([-4, -3, -2, -1, 0, 1, 2, 3, 4], [16, 9, 4, 1, 0, 1, 4, 9, 16])
+plt.savefig('fig_2.png')
+
