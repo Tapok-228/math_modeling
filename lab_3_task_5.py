5270592b53a9dbdd632d757249f5c21b3da2decd
Tapok-228 <153547706+Tapok-228@users.noreply.github.com>
2024-03-10 11:15:40 +0000

    Доделал ДЗ

--- lab_3_task_5.py
+++ lab_3_task_5.py
@@ -1,5 +1,5 @@
 from lab_3_task_4 import triganometry_array, m
 
-triganometry_array[:, [int(i) for i in range(m)]] = triganometry_array[:, [1, 0, 3, 2]]  # надо что-то решить с последовательностью замены
+triganometry_array[:, [0, 1]] = triganometry_array[:, [1, 0]]
 
 print(triganometry_array)
