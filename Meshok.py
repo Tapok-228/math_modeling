61d27ef86fd356fc8c83b76c0e86ce01b6b6c2b9
Tapok-228 <153547706+Tapok-228@users.noreply.github.com>
2024-04-07 16:46:54 +0000

    meshok_splain

--- Meshok.py
+++ Meshok.py
@@ -18,20 +18,21 @@ def circle_r(R, x0, y0, t0, t1):
     y = R * np.sin(t)+y0
     return x, y
 
-coords_line = np.array([[870, 890], [805, 630]])
-coords_line_1 = np.array([[1000, 1200], [805, 630]])
+coords_line = np.array([[250, 290], [100, 110]])
+coords_line_1 = np.array([[291, 300], [111, 180]])
 x = np.append(coords_line[0], coords_line_1[0])
 y = np.append(coords_line[1], coords_line_1[1])
 
 
+coords_line = np.array([[299, 235], [179, 135]])
+x = np.append(x, coords_line[0])
+y = np.append(y, coords_line[1])
 
 
 
-
-
-
-
-
+coords_line = np.array([[236, 250], [136, 100]])
+x = np.append(x, coords_line[0])
+y = np.append(y, coords_line[1])
 
 
 
